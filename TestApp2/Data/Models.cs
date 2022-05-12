45f8204a4cfcdaad1a2a380caddfcce1989aab54
Diaskhan <diaskhan@yandex.kz>
2022-05-12 19:56:59 +0600

    added models and ef core nuget

---
+++ TestApp2/Data/Models.cs
@@ -0,0 +1,26 @@
+﻿namespace TestApp2.Data
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Decription { get; set; }
+        public int Priority { get; set; }
+
+        public int Status { get; set; }
+
+        //enum: ToDo / InProgress / Done
+    }
+
+    public class Project
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime CompleteDate { get; set; }
+        public int Status { get; set; }
+        
+        //the current status of the project (enum: NotStarted, Active, Completed)
+
+    }
+}
